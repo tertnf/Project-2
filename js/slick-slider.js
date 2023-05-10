f9ca5f2c1f59a43757ee20dfa0ec1f7b4a1aba42
Nataliia Tertyshna <tertnf@gmail.com>
2023-05-10 13:45:51 +0300

    nav, hero, about mobile done
    nav, hero, about mobile done

---
+++ js/slick-slider.js
@@ -0,0 +1,87 @@
+$(document).ready(function () {
+  $(".youtube-slider").slick({
+    arrows: false,
+    // appendArrows: $(".arrows-feedback-slider"),
+    infinite: true,
+    speed: 500,
+    easing: "ease",
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: ".describe-slider",
+
+    // variableWidth: true,
+    // adaptiveHeight: true,
+    // centerMode: true,
+    // rows: 1,
+    // responsive: [
+    // {
+    //   breakpoint: 600,
+    //   settings: {
+    //     slidesToShow: 2,
+
+    //     slidesToScroll: 1,
+    //   },
+    // },
+    // {
+    //   breakpoint: 400,
+    //   settings: {
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //   },
+    // },
+    //   {
+    //     breakpoint: 320,
+    //     settings: {
+    //       slidesToShow: 1,
+    //       slidesToScroll: 1,
+    //     },
+    //   },
+    // ],
+  });
+  $(".describe-slider").slick({
+    arrows: false,
+    // appendArrows: $(".arrows-feedback-slider"),
+    infinite: true,
+    speed: 500,
+    easing: "ease",
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: ".youtube-slider",
+
+    // variableWidth: true,
+    // adaptiveHeight: true,
+    // centerMode: true,
+    // rows: 1,
+    // responsive: [
+    // {
+    //   breakpoint: 600,
+    //   settings: {
+    //     slidesToShow: 2,
+
+    //     slidesToScroll: 1,
+    //   },
+    // },
+    // {
+    //   breakpoint: 400,
+    //   settings: {
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //   },
+    // },
+    //   {
+    //     breakpoint: 320,
+    //     settings: {
+    //       slidesToShow: 1,
+    //       slidesToScroll: 1,
+    //     },
+    //   },
+    // ],
+  });
+});
+
+$(".video-controls-arrow-left").click(function (event) {
+  $(".youtube-slider").slick("slickPrev");
+});
+$(".video-controls-arrow-right").click(function (event) {
+  $(".youtube-slider").slick("slickNext");
+});
